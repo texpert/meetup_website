ad60210c0f852a6e4e43bb333de173adf39f4f1b
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-07-30 23:28:25 +0300

    Pin actioncable, activestorage, actiontext, and trix packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin "@rails/activestorage", to: "activestorage.esm.js"
+pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "trix"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
