8ad9eb90d9e6b891775673c3791eb066ef5618bd
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-07-31 19:00:39 +0300

    Fix bootstrap.js link in importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,5 +9,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js", preload: true
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css", preload: true
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
