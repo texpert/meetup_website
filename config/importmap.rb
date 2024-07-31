0b7c674c45fe3640382e2688f15779ebda0b559d
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-07-31 16:34:06 +0300

    Add bootstrap 5.3.3 and popperjs 2.11.8

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,6 @@ pin "trix"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js", preload: true
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
