cbb67072b48c440365bde8d37c0e22fb6d16397f
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-02 17:42:08 +0300

    Add curent_user helper to use instead of current_account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def current_user
+    rodauth.rails_account
+  end
+  helper_method :current_user
+
+  def authenticate
+    rodauth.require_account
+  end
 end
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,17 +1,34 @@
 # frozen_string_literal: true
 
 class SubscriptionsController < ApplicationController
+  before_action :set_subscription, only: [:show, :edit, :update, :destroy]
+
   def index
-    @subscriptions = current_account.subscriptions.all
+    @subscriptions = current_user.subscriptions.all
+  end
+
+  def show
+  end
+
+  def new
+    @subscription = current_user.subscriptions.new
+  end
+
+  # GET /posts/1/edit
+  def edit
   end
 
   def create
-    @subscription = current_account.subscriptions.build(subscription_params)
+    @subscription = current_user.subscriptions.new(subscription_params)
   end
 
   private
 
   def set_subscription
-    @subscription = current_account.subscriptions.find(params[:id])
+    @subscription = current_user.subscriptions.find(params[:id])
+  end
+
+  def subscription_params
+    params.require(:subscription).permit(:name, :plan_id)
   end
 end
