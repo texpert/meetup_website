677dd0367d29ab1a2c2b909ebcf04e38c7027c86
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-02 23:48:31 +0300

    Add Email model and User has_many :emails, as: :owner

---
+++ app/models/email.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Email < ApplicationRecord
+  KIND = { contact: 1, recovery: 2, alternative: 3 }.freeze
+
+  include Discard::Model
+
+  attr_readonly :account_email
+
+  enum :status, unverified: 1, verified: 2, unreachable: 3
+
+  belongs_to :owner, -> { with_discarded }, polymorphic: true
+
+  validate kind_values_legit, on: %i[create update]
+
+  def kind_values_legit
+    errors.add(:kind, :invalid) if kind_changed? && !kind_before_type_cast.is_a?(Array)
+    errors.add(:kind, :invalid) unless (kind - KIND.values).empty?
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   include Rodauth::Rails.model
   include Discard::Model
@@ -6,7 +8,14 @@ class User < ApplicationRecord
 
   enum :status, staged: 1, unverified: 2, active: 3, recovery: 4, expired: 5, locked: 6, suspended: 7, disabled: 8
 
+  has_many :emails, as: :owner, dependent: :destroy
+  accepts_nested_attributes_for :emails
+
   has_many :subscriptions, -> { with_discarded }, as: :owner, dependent: :destroy
+
+  def email
+    @email ||= emails.find_by(account_email: true)
+  end
 
   def plan
     subscriptions.active.first&.plan
