d43b8dbed2e891a88745a777c2b252883485cc6e
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-06 23:39:30 +0300

    Pin bootstrap and popplerjs

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,6 +8,8 @@ pin "trix"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js", preload: true
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js", preload: true
+
+# bin/importmap pin @popperjs/core@2.11.8/+esm --from jsdelivr and then rename the downloaded file removing the +
+pin "@popperjs/core", to: "@popperjs--core--esm.js" # @2.11.8
+pin "bootstrap" # @5.3.3
 pin_all_from "app/javascript/controllers", under: "controllers"
