69989e637604d37b8b98c789430c9df71a39da7c
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-08 14:22:00 +0300

    Ovveride destroy on users/registrations_controller to discard resource, instead of destroying it

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -25,9 +25,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    resource.discard
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message! :notice, :destroyed
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name), status: Devise.responder.redirect_status }
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
