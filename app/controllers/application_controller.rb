0932476488a9159894eaf8e653a1d7eee55d716c
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-08 14:39:44 +0300

    Add around_action :add_logidze_meta to the ApplicationController for logging the data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  around_action :add_logidze_meta, only: %i[create update]
+
+  def add_logidze_meta(&block)
+    Logidze.with_meta({ ip: request.ip, responsible: current_user&.id }, &block)
+  end
 end
