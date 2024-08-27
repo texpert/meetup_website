7be539148874d02d1d0ecdf876d8e219212e2f93
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-27 16:25:11 +0300

    Add the :turbo_frame_request_variant before_action to ApplicationController to have a single controller action respond to both Turbo Frame requests and regular, full-page requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@
 
 class ApplicationController < ActionController::Base
   around_action :add_logidze_meta, only: %i[create update] # rubocop:disable Rails/LexicallyScopedActionFilter
+  before_action :turbo_frame_request_variant
 
   def add_logidze_meta(&)
     Logidze.with_meta({ ip: request.ip, responsible: current_user&.id }, &)
+  end
+
+  def turbo_frame_request_variant
+    request.variant = :turbo_frame if turbo_frame_request?
   end
 end
