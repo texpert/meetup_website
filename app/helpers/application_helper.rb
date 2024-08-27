e9d169b18fef34197847210064af45eb8b4e2908
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-27 16:21:15 +0300

    Added the `render_modal` helper so it's convenient to render the modal partial in the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def render_modal(title: '', body: '', footer: '')
+    render(partial: '/partials/modal', locals: { title:, body:, footer: })
+  end
 end
