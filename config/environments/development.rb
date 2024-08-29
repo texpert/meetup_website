05578c500030d205013b7369d14f6daea056abe4
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-29 15:42:33 +0300

    Set config.hosts << "meetup.dev:3000" in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  config.hosts << "meetup.dev:3000"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
