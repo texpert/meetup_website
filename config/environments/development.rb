6ff931f4fbb011eefac07fcda89aedd2ad6dc04b
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-29 15:42:04 +0300

    Configure action_mailer.smtp_settings, asset_host, and delivery_method for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,13 +37,22 @@ Rails.application.configure do
   # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+
+  # Configured for the mailcatcher gem
+  config.action_mailer.default_url_options = { port: 3000, host: 'meetup.dev' }
+  config.action_mailer.asset_host = 'http://meetup.dev:3000'
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
