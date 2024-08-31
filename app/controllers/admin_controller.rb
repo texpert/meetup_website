8a27b248ca6d889887b431736f74d9e487469286
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-08-31 14:27:26 +0300

    Add dashboard and index actions, admin layout to AdminController

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class AdminController < ApplicationController
+  layout 'admin'
+
+  # render admin dashboard
+  def index
+    render 'dashboard'
+  end
+
+  def dashboard
+    index
+  end
 end
