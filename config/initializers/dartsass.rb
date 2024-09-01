00cf72f391a815d09eebe3c6f4a2fdec4a2326de
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-09-01 19:32:47 +0300

    Add comment into the dartsass initializer about processing allowance for all the files from the app/assets/stylesheets folder

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Allow processing of all the scss files from app/assets/stylesheets - by default `dartsass-rails` is processing only
+# the `application.scss` file
 Rails.application.config.dartsass.builds = {
   '.' => '.'
 }
