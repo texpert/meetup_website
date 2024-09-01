9e204b093c3918a65417c3f21ee2cfa3c43cbd89
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-09-01 19:39:47 +0300

    Add disable_link_if_page helper method to help making the currently selected link visibly active and interactionally non-active (non-clickable, non-touchable, non-selectable by Tab key)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
   def render_modal(title: '', body: '', footer: '')
     render(partial: '/partials/modal', locals: { title:, body:, footer: })
   end
+
+  def disable_link_if_page(page, tag_class: '')
+    if current_page?(page)
+      { class: "#{tag_class} active", style: 'pointer-events: none; touch-action: none', tabindex: -1 }
+    else
+      { class: tag_class, style: 'pointer-events: inherit; touch-action: inherit', tabindex: 0 }
+    end
+  end
 end
