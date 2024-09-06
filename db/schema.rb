f4065b59d3bb44ea08a5dc851ad0ca9c55c0904e
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-09-06 09:58:55 +0300

    Refactor solid_cache to its defaults - dedicate DB for cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_08_101937) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_08_101937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "hstore"
@@ -42,17 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_08_101937) do
     t.index ["user_id", "user_type", "primary"], name: "index_emails_on_user_id_user_type_and_primary"
   end
 
-  create_table "solid_cache_entries", force: :cascade do |t|
-    t.binary "key", null: false
-    t.binary "value", null: false
-    t.datetime "created_at", null: false
-    t.bigint "key_hash", null: false
-    t.integer "byte_size", null: false
-    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
-    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
-    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.enum "status", default: "staged", null: false, enum_type: "user_status"
     t.string "encrypted_password", default: "", null: false
@@ -75,7 +64,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_08_101937) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["status"], name: "index_users_on_status"
   end
-
   create_function :logidze_capture_exception, sql_definition: <<-'SQL'
       CREATE OR REPLACE FUNCTION public.logidze_capture_exception(error_data jsonb)
        RETURNS boolean
