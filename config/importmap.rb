53d17728f70c8c861097aaef310bd8fee3556f67
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2025-05-06 19:44:57 +0300

    Bump Bootstrap to 5.3.6

--- config/importmap.rb
+++ config/importmap.rb
@@ -13,7 +13,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 # bin/importmap pin @popperjs/core@2.11.8/+esm --from jsdelivr and then rename the downloaded file removing the +
 pin "@popperjs/core", to: "@popperjs--core--esm.js" # @2.11.8
-pin "bootstrap" # @5.3.4
+pin "bootstrap" # @5.3.6
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.7.2
 pin "@fortawesome/fontawesome-svg-core", to: "@fortawesome--fontawesome-svg-core.js" # @6.7.2
