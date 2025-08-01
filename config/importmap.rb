9a92ee5d9f5aaf0c8d0f1edb848b4dbb51a06936
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2025-08-01 12:05:36 +0300

    Bump @fortawesome/* packages to 7.0.0

--- config/importmap.rb
+++ config/importmap.rb
@@ -15,9 +15,9 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@popperjs/core", to: "@popperjs--core--esm.js" # @2.11.8
 pin "bootstrap" # @5.3.7
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.7.2
-pin "@fortawesome/fontawesome-svg-core", to: "@fortawesome--fontawesome-svg-core.js" # @6.7.2
-pin "@fortawesome/free-brands-svg-icons", to: "@fortawesome--free-brands-svg-icons.js" # @6.7.2
-pin "@fortawesome/free-regular-svg-icons", to: "@fortawesome--free-regular-svg-icons.js" # @6.7.2
-pin "@fortawesome/free-solid-svg-icons", to: "@fortawesome--free-solid-svg-icons.js" # @6.7.2
+pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @7.0.0
+pin "@fortawesome/fontawesome-svg-core", to: "@fortawesome--fontawesome-svg-core.js" # @7.0.0
+pin "@fortawesome/free-brands-svg-icons", to: "@fortawesome--free-brands-svg-icons.js" # @7.0.0
+pin "@fortawesome/free-regular-svg-icons", to: "@fortawesome--free-regular-svg-icons.js" # @7.0.0
+pin "@fortawesome/free-solid-svg-icons", to: "@fortawesome--free-solid-svg-icons.js" # @7.0.0
 pin "admin-lte" # @4.0.0
